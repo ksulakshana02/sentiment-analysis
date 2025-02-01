3823a39e0e7107f8d62d4c7c0e155d5392fbaf9d
ksulakshana02 <sulakshanakaveesha@gmail.com>
2025-02-01 17:03:35 +0530

    create web app

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, request, redirect
+from helper import preprocessing, vectorizer, get_prediction
+
+app = Flask(__name__)
+
+data = dict()
+reviews = []
+positive = 0
+negative = 0
+
+@app.route("/")
+def index():
+    data['reviews'] = reviews
+    data['positive'] = positive
+    data['negative'] = negative
+    return render_template('index.html', data=data)
+
+
+@app.route("/", methods=["POST"])
+def my_post():
+    text = request.form['text']
+    preprocessed_text = preprocessing(text)
+    vectorized_text = vectorizer(preprocessed_text)
+    prediction = get_prediction(vectorized_text)
+
+    if prediction == 'negative':
+        global negative
+        negative += 1
+    else:
+        global positive
+        positive += 1
+    
+    reviews.insert(0,text)
+    return redirect(request.url)
+
+if __name__ == "__main__":
+    app.run()
+
